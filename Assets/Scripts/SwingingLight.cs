3239a46570f9955852c6810c25dbe579d43326c6
Adam Keenan <adam_keenan@taylor.edu>
2014-11-21 21:59:31 -0500

    Things

---
+++ Assets/Scripts/SwingingLight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingingLight : MonoBehaviour {
+
+    private int dir = 1;
+    private float speed = 2f;
+
+    // Use this for initialization
+    void Start () {
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        this.transform.position = new Vector3 (this.transform.position.x + this.speed * this.dir * Time.deltaTime, this.transform.position.y, this.transform.position.z);
+        if (this.transform.position.x < -10 && this.dir == -1)
+            this.dir = 1;
+        else if (this.transform.position.x > 10 && this.dir == 1) {
+            this.dir = -1;
+        }
+    }
+}
